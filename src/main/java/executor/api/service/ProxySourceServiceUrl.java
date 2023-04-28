b96124e98aea660e242a92a7b8147386141c48d6
Serhii Matvienko <111274705+Serhx4@users.noreply.github.com>
2023-04-28 19:33:34 +0300

    Revert "Create Service ProxySourceServiceUrl (#13)" (#18)
    This reverts commit ddf5a222379d60753982a682c18a6667bd3d9cf3.

--- src/main/java/executor/api/service/ProxySourceServiceUrl.java
+++
@@ -1,7 +0,0 @@
-package executor.api.service;
-
-import com.solacesystems.jcsmp.JCSMPException;
-
-public interface ProxySourceServiceUrl {
-    void sendMessage(String message) throws JCSMPException;
-}
