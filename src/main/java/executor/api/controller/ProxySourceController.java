02807eae08cf9dfaa495d213f8e783916e67c81f
Kirill <47983936+Duskmage2009@users.noreply.github.com>
2023-05-02 11:56:55 +0300

    Create Proxy Source Controller (#15)
    * Create Proxy Source Controller
    
    * Added @GetMapping to service
    
    * Added check for exeption in Get method
    
    * Added ResponceStatusException in Get method
    
    * Added HttpStatus.CREATED  in Post method

---
+++ src/main/java/executor/api/controller/ProxySourceController.java
@@ -0,0 +1,38 @@
+package executor.api.controller;
+
+import executor.api.model.ProxyConfigHolderDTO;
+import executor.api.service.impl.ProxySourceQueueHandler;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/api/v1/proxies")
+
+public class ProxySourceController {
+
+    private final ProxySourceQueueHandler proxySourceQueueHandler;
+
+    public ProxySourceController(ProxySourceQueueHandler proxySourceQueueHandler) {
+        this.proxySourceQueueHandler = proxySourceQueueHandler;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> addProxy(@RequestBody ProxyConfigHolderDTO proxyConfigHolderDTO) {
+        proxySourceQueueHandler.add(proxyConfigHolderDTO);
+        return new ResponseEntity<>("Added successfully",HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ProxyConfigHolderDTO getProxy() {
+        Optional<ProxyConfigHolderDTO> optional = proxySourceQueueHandler.poll();
+       return optional.orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+}
+
+
+
