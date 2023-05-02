7f6c950d751ea08d8cd9f5efef1696164fb8f414
bielik-b <112386691+bielik-b@users.noreply.github.com>
2023-05-02 10:17:40 +0300

    Create ScenarioSourceController (#17)

---
+++ src/main/java/executor/api/controller/ScenarioSourceController.java
@@ -0,0 +1,31 @@
+package executor.api.controller;
+
+import executor.api.model.ScenarioDTO;
+import executor.api.service.ScenarioSourceQueueHandlerImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/scenarios")
+public class ScenarioSourceController {
+   private final ScenarioSourceQueueHandlerImpl scenarioSourceQueueHandler;
+
+    public ScenarioSourceController(ScenarioSourceQueueHandlerImpl scenarioSourceQueueHandler) {
+        this.scenarioSourceQueueHandler = scenarioSourceQueueHandler;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> offerScenario(@RequestBody ScenarioDTO scenarioDTO){
+        scenarioSourceQueueHandler.offer(scenarioDTO);
+       return new ResponseEntity<String>("Scenario is successfully created", HttpStatus.CREATED);
+    }
+    @GetMapping
+    public ScenarioDTO getScenario(){
+        Optional<ScenarioDTO> optional = Optional.ofNullable(scenarioSourceQueueHandler.poll());
+        return optional.orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+}
