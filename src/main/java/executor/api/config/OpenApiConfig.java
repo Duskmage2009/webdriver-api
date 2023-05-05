f98ec877c4d171cfbbf42c4d125fada4e2ec2db1
Serhii Matvienko <111274705+Serhx4@users.noreply.github.com>
2023-05-05 21:07:56 +0300

    Update config OpenApiConfig with server (#35)

--- src/main/java/executor/api/config/OpenApiConfig.java
+++ src/main/java/executor/api/config/OpenApiConfig.java
@@ -3,8 +3,11 @@ package executor.api.config;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -18,7 +21,12 @@ public class OpenApiConfig {
                         .version("v1"))
                 .externalDocs(new ExternalDocumentation()
                         .description("Executor-Service github")
-                        .url("https://github.com/GFLcourses3/group1-microservice2"));
+                        .url("https://github.com/GFLcourses3/group1-microservice2"))
+                .servers(List.of(
+                        new Server()
+                                .url("https://gfl-course-group1.up.railway.app")
+                                .description("api server")
+                ));
     }
 
 }
